bdeff556384cd173bd128f67a65724bccd7a284b
Soumyadeepkoley <66461161+Soumyadeepkoley@users.noreply.github.com>
2022-04-14 14:37:06 +0530

    Create Code7.java

---
+++ Soumyadeep Koley/Code7.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the color: ");
+		String q=sc.next();
+		if(q.equals("Green")||q.equals("green"))
+		{
+			System.out.print("Go");	
+		}
+		else if(q.equals("yellow")||q.equals("Yellow"))
+		{
+			System.out.print("proceed with caution");	
+		}
+		else if(q.equals("Red")||q.equals("red"))
+		{
+			System.out.print("Stop");	
+		}
+		else
+		{
+			System.out.print("prepare to go");
+		}
+		
+	}
+
+}
