6c9e3b9a295a89ad8fd430d740d5256a0a36c434
THIRUKUMARAN14 <103258849+THIRUKUMARAN14@users.noreply.github.com>
2022-04-14 16:25:56 +0530

    Create Ex9.java

---
+++ Thirukumaran/12April22/Assignmentjava/Ex9.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int grade=s.nextInt();
+		if(grade>=0 && grade<=4){
+		    System.out.println("fail");
+		}
+	    else if(grade == 5){
+	        System.out.println("Approved");
+	    }
+	    else if(grade == 6){
+	        System.out.println("Good");
+	    }
+	    else if(grade==7 || grade==8){
+	        System.out.println("Notable");
+	    }
+	    else if(grade==9 || grade==10){
+	        System.out.println("Excellent");
+	    }
+	    else{
+	        System.out.println("Invalid");
+	    }
+	}
+}
+____________________________________________________
