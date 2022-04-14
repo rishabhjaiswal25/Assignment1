e0dd0f2629a2fb20d8a0216fb70830b5ce93b6ca
THIRUKUMARAN14 <103258849+THIRUKUMARAN14@users.noreply.github.com>
2022-04-14 16:21:36 +0530

    Create Ex5.java

---
+++ Thirukumaran/12April22/Assignmentjava/Ex5.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.print("Enter the cost of the mobile : ");
+		int cost=s.nextInt();
+		if(cost<=15000){
+		    System.out.println("Mobile chosen is within the budget");
+		}
+		else{
+		    System.out.println("Mobile chosen is beyond the budget");
+		}
+	}
+}
+
