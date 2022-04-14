e8969c280e41b95750c729feb3eede187262264f
vasudevan <75119057+vasudevan037@users.noreply.github.com>
2022-04-14 16:45:18 +0530

    Add files via upload

---
+++ Vasudevan Assignment/Q4.Teen Number.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Main {
+public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+		System.out.print("a : ");
+		int a=s.nextInt();
+		System.out.print("b : ");
+		int b=s.nextInt();
+		if((a>=13 && a<=19)||(b>=13 && b<=19)){
+		    System.out.println("teen sum : 19");
+		}
+		else{
+		    System.out.println("teen sum : "+(a+b));
+		}
+	}
+}
