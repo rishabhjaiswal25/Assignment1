5c3f2a9d9deeec0598f4b1c3a8bef693208648d1
Dhanumalayan <103258269+Dhanumalayan@users.noreply.github.com>
2022-04-14 16:13:18 +0530

    Create ex2.java

---
+++ Dhanumalayan/12April22/Assignmentjava/ex2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void Cal_expenses(int a,double b){
+    double cost=a*b;
+    if(a>1000){
+        cost=cost-(cost/10);
+    }
+    System.out.println("Total Expenses : "+cost);
+    }
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.print("Enter Qantity : ");
+		int a=s.nextInt();
+		System.out.print("Enter Rate per item : ");
+		double b=s.nextDouble();
+		Cal_expenses(a,b);
+	}
+}
