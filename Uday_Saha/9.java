1be6ba2cd52cfdbc1b638a58c163828e7dfbb371
Sahauday <103258047+Sahauday@users.noreply.github.com>
2022-04-15 12:12:34 +0530

    Create 9.java

---
+++ Uday_Saha/9.java
@@ -0,0 +1,40 @@
+package co.solution;
+import java.util.*;
+public class Q9 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number");
+		int n=sc.nextInt();
+		if(n>=9 && n<=10)
+		{
+			System.out.println("Excellent");
+		}
+		else if(n>=7 && n<=8 )
+		{
+			System.out.println("Notable");
+		}
+		else if(n==6 )
+		{
+			System.out.println("Good");
+		}
+		else if(n==5 )
+		{
+			System.out.println("Approved");
+		}
+		else if(n>=1 && n<=4 )
+		{
+			System.out.println("Fail");
+		}
+		else if(n>=1 && n<=4 )
+		{
+			System.out.println("Fail");
+		}
+		else
+		{
+			System.out.println("Invalid Input");
+		}
+
+	}
+
+}
